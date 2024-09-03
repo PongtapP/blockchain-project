9c50ad4bf1c12a612a09600fd8e077b6770f6351
PongtapP <653070310210@npu.ac.th>
2024-09-03 14:22:03 +0700

    Start

--- heritage.sol
+++ heritage.sol
@@ -7,5 +7,10 @@ contract heritage {
         owner = msg.sender;
     }
 
-    
+    //1.put money in to pool
+    //2.view pool balance
+    //3.add/remove person
+    //4.view person
+    //5.distribute inheritage
+    //6.keep alive
 }
